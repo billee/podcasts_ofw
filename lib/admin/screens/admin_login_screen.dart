c1ff64260d2f006bee8f66e1c0d5acb86ed195fe
billee <twilly.t@gmail.com>
2025-10-07 22:10:31 -0400

    player is working and gradle issue was fixed

---
+++ lib/admin/screens/admin_login_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class AdminLoginScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Admin Login')),
+      body: Center(child: Text('Admin Login Screen - To be implemented')),
+    );
+  }
+}
